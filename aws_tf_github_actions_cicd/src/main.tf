c26befdad9cad0c5bca79130f8768e3b4113f035
Piyush Kumar <piyushk@vdi-bglddp-068.dhcp.mathworks.com>
2024-06-20 13:30:12 +0530

    adding the issue

--- aws_tf_github_actions_cicd/src/main.tf
+++ aws_tf_github_actions_cicd/src/main.tf
@@ -1,4 +1,14 @@
 terraform {
+  # Run init/plan/apply with "backend" commented-out (ueses local backend) to provision Resources (Bucket, Table)
+  # Then uncomment "backend" and run init, apply after Resources have been created (uses AWS)
+  /* backend "s3" {
+    bucket         = "cc-tf-state-backend-local-ci-cd"
+    key            = "tf-infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  } */
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -13,6 +23,6 @@ provider "aws" {
 
 module "tf-state" {
   source      = "../modules/tf-state"
-  bucket_name = "cc-tf-state-backend-local-ci-cd"
+  bucket_name = "cc-tf-state-backend-local-ci-cd1"
 }
 
