d374b8baf41eeffe1b5bd381888f722b18d94708
Piyush Kumar <piyushk@vdi-bglddp-068.dhcp.mathworks.com>
2024-06-21 11:05:26 +0530

    including github actions and s3 backend

--- aws_tf_github_actions_cicd/src/main.tf
+++ aws_tf_github_actions_cicd/src/main.tf
@@ -1,7 +1,7 @@
 terraform {
   # Run init/plan/apply with "backend" commented-out (ueses local backend) to provision Resources (Bucket, Table)
   # Then uncomment "backend" and run init, apply after Resources have been created (uses AWS)
-  /* backend "s3" {
+   /*backend "s3" {
     bucket         = "cc-tf-state-backend-local-ci-cd"
     key            = "tf-infra/terraform.tfstate"
     region         = "us-east-1"
@@ -26,3 +26,12 @@ module "tf-state" {
   bucket_name = "cc-tf-state-backend-local-ci-cd"
 }
 
+module "vpc-infra" {
+  source = "../modules/vpc"
+
+  # VPC Input Vars
+  vpc_cidr             = local.vpc_cidr
+  availability_zones   = local.availability_zones
+  public_subnet_cidrs  = local.public_subnet_cidrs
+  private_subnet_cidrs = local.private_subnet_cidrs
+}
